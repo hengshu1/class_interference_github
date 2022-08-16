670960dd6afa552b8718b20f6341e525b4b855fa
hengshu1 <hengshu1@ualberta.ca>
2022-08-16 15:04:09 -0600

    removing plots of f for the other alpha; only use alpha=0.001

--- plot_f.py
+++ plot_f.py
@@ -5,9 +5,9 @@ import numpy as np
 f_loss01 = np.load('results/f_vgg_sgd_alpha_'+str(0.1)+'.npy')
 f_loss001 = np.load('results/f_vgg_sgd_alpha_'+str(0.01)+'.npy')
 f_loss0001 = np.load('results/f_vgg_sgd_alpha_'+str(0.001)+'.npy')
-print('f_loss001.shape=',f_loss001.shape)
+print('f_loss001.shape=', f_loss001.shape)
 
-rho01,rho001,rho0001 = [], [], []
+rho01, rho001, rho0001 = [], [], []
 for i in range(len(f_loss01)):
     rho01.append(f_loss01[i]/f_loss01[0])
 
@@ -17,10 +17,11 @@ for i in range(len(f_loss001)):
 for i in range(len(f_loss0001)):
     rho0001.append(f_loss0001[i]/f_loss0001[0])
 
-pt.plot(rho01, '-b',label='lr=0.1')
-pt.plot(rho001, '--r', label='lr=0.01')
-pt.plot(rho0001, '-.k', label='lr=0.001')
+# pt.plot(rho01, '-b',label='lr=0.1')
+# pt.plot(rho001, '--r', label='lr=0.01')
+# pt.plot(rho0001, '-r', label='lr=0.001')
+pt.plot(rho0001, '-r', label=r'$\rho_t$')
 pt.legend(fontsize=20)
-pt.xlabel(r'$t$'+'(epochs)',fontsize=20)
-pt.ylabel(r'$\rho_t$',fontsize=20)
+pt.xlabel(r'$t$'+'(epochs)', fontsize=20)
+pt.ylabel(r'$\rho_t$', fontsize=20)
 pt.show()
