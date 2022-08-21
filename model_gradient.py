ea1f83d81b6bf8f8a060f3cbc6695c2402171c6a
hengshuai yao <hengshu1@ualberta.ca>
2022-08-21 12:26:01 -0600

    Why the two gradients are different????

--- model_gradient.py
+++ model_gradient.py
@@ -17,21 +17,25 @@ from torch.nn.utils import parameters_to_vector as to_vector
 # from .measure_cross_class_distances import get_train_cats
 from main import device
 
+torch.manual_seed(0)
+
 def concat_param_grad(net):
     '''
     fix x = net; we are interested at the final model centered. compute f'(x)
     And check the curvatures around it; and see any direction going down? any leaky direction? If mu<0, then it's leaky
     todo: follow https://discuss.pytorch.org/t/get-the-gradient-of-the-network-parameters/50575/2
     '''
-    first_time = True
+    wg_all = None
     for name, param in net.named_parameters():
+        # print('name=', name)
         # print(name, param.size())
         # print(name, 'grad size:', param.grad.size())
+        # print('param.grad.shape=', param.grad.shape)
+        # if name == 'module.features.0.weight':
+        #     print('concat_param_grad: param.grad=', param.grad.cpu().numpy()[:3, 0, 0, 0])
         _wgrad = to_vector(param.grad)
-        if first_time:
+        if wg_all is None:
             wg_all = _wgrad
-            first_time = False
-        # print('1d len:', _wgrad.size())
         else:
             wg_all = torch.cat((wg_all, _wgrad))
 
@@ -43,80 +47,96 @@ def concat_param_grad(net):
 
 def aver_grad_1D(trainloader, net, optimizer, criterion):
     '''average the gradient into 1D vector'''
-    net.train()
+    # net.train()
     train_loss = 0
     correct = 0
     total = 0
 
-    first_time = True
+    grads_sum = None
     for batch_idx, (inputs, targets) in enumerate(trainloader):
         inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad()
         outputs = net(inputs)
         # print('targets=', targets)
         loss = criterion(outputs, targets)
-        loss.backward()
-        # optimizer.step()
+        loss.backward()#compute gradient
+        # optimizer.step()#no model update
+
+        if batch_idx == 0:
+            print('targets=', targets)
+            for name, param in net.named_parameters():
+                print('name=', name)
+                print(param.grad[:3, 0, 0, 0])
+                sys.exit(1)
 
         train_loss += loss.item()
         _, predicted = outputs.max(1)
         total += targets.size(0)
         correct += predicted.eq(targets).sum().item()
 
-        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                     % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+        # progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+        #              % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
         grads = concat_param_grad(net)
-        if first_time:
+        if grads_sum is None:
             grads_sum = grads
-            first_time = False
         else:
             grads_sum += grads
 
     grads_sum /= total
     print('total samples=', total)
-    return grads_sum.cpu().numpy()
+    return grads_sum
 
 
 
 def aver_grad_net(trainloader, net, optimizer, criterion):
     '''average the gradient and keeps it in a model form'''
-    net.train()
+    # net.train()
     train_loss = 0
     correct = 0
     total = 0
+
+    grads_sum = {}
+    for name, param in net.named_parameters():
+        grads_sum[name] = None
+    # print('grads_sum=', grads_sum)
 
     for batch_idx, (inputs, targets) in enumerate(trainloader):
         inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad()
         outputs = net(inputs)
-        # print('targets=', targets)
         loss = criterion(outputs, targets)
         loss.backward()
         # optimizer.step()
+
+        if batch_idx == 0:
+            print('targets=', targets)
+            for name, param in net.named_parameters():
+                print('name=', name)
+                print(param.grad[:3, 0, 0, 0])
+                break
 
         train_loss += loss.item()
         _, predicted = outputs.max(1)
         total += targets.size(0)
         correct += predicted.eq(targets).sum().item()
 
-        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                     % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+        # progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+        #              % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
-        grads_sum = dict.fromkeys(net.named_parameters().keys())
-        print('grads_sum=', grads_sum)
-        sys.exit(1)
         for name, param in net.named_parameters():
-            if grads_sum is None:
+            # print('name=', name)
+            # print('param.grad=', param.grad.cpu().numpy()[:3, 0, 0, 0])
+            if grads_sum[name] is None:
                 grads_sum[name] = param.grad
             else:
                 grads_sum[name] += param.grad
 
     print('total samples=', total)
-    for name, _ in net.named_parameters():
+    for name in grads_sum.keys():
         grads_sum[name] /= total
 
-    return grads_sum.cpu().numpy()
+    return grads_sum
 
 
 if __name__ == "__main__":
@@ -137,16 +157,18 @@ if __name__ == "__main__":
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     net = net.to(device)
-    if device == 'cuda':
-        net = torch.nn.DataParallel(net)
-        cudnn.benchmark = True
+    # if device == 'cuda':
+        # net = torch.nn.DataParallel(net)
+        # cudnn.benchmark = True
+
+    net1 = copy.deepcopy(net)
 
     # model_path = 'results/model_vgg_sgd_alpha_'+str(0.001)
     model_path = 'results/model_vgg_sgd_alpha_'+str(0.01)
     # model_path = 'results/model_vgg_sgd_alpha_'+str(0.001)+'_batchsize1024'
     # model_path = 'results/model_vgg_sgd_alpha_'+str(0.01)+'_batchsize1024'
-    print('loading model at path:', model_path)
-    net.load_state_dict(torch.load(model_path+'.pyc'))
+    # print('loading model at path:', model_path)
+    # net.load_state_dict(torch.load(model_path+'.pyc'))
     # print(net)
 
     # Data
@@ -160,18 +182,33 @@ if __name__ == "__main__":
     ])
 
     criterion = nn.CrossEntropyLoss(reduction='sum')  # by default. it's mean.
-    optimizer = optim.SGD(net.parameters(), lr=args.lr,
-                          momentum=0, weight_decay=0)  # first do without momentum
+    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0, weight_decay=0)  # first do without momentum
 
-    trainset = torchvision.datasets.CIFAR10(
-        root='./data', train=True, download=True, transform=transform_train)
-    trainloader = torch.utils.data.DataLoader(
-        trainset, batch_size=args.batchsize, shuffle=True, num_workers=2)
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batchsize, shuffle=False, num_workers=2)
 
-    #only one epoch
-    # grad = aver_grad_1D(trainloader, net, optimizer, criterion)
-    grad = aver_grad_net(trainloader, net, optimizer, criterion)
-    print('grads.shape=', grad.shape)
+    print('aver_grad_net')
+    grad_net = aver_grad_net(trainloader, net, optimizer, criterion)
+
+    #reset the model
+    net2 = net1
+    optimizer2 = optim.SGD(net2.parameters(), lr=args.lr, momentum=0, weight_decay=0)
+    criterion2 = nn.CrossEntropyLoss(reduction='sum')
+
+    print('@@@@')
+    print('aver_grad_1D')
+    grad_1D = aver_grad_1D(trainloader, net2, optimizer2, criterion2)
+    print('grad_1D.shape=', grad_1D.shape)
+
+    #why the two gradients are different????
+    # grads = []
+    # for name in grad_net.keys():
+    #     grads.append(grad_net[name].view(-1))
+    #     # grads.append(to_vector(grad_net[name]))
+    # grads = torch.cat(grads)
+
+    # print('grad diff=', torch.norm(grads - grad_1D))
+
     # np.save(model_path+'_grad.npy', grad)
 
     # class_loader = get_train_cats(trainset, batch_size=1024, label=3)  # get cats
